cf343ff2c6badf516b683bd06557da8e4ae36d6a
kaiet <kjimeneald@educacion.navarra.es>
2023-12-22 10:33:04 +0100

    Acabado

--- Ejercicio1/app/src/main/java/com/example/ejercicioappimc/MainActivity.kt
+++ Ejercicio1/app/src/main/java/com/example/ejercicioappimc/MainActivity.kt
@@ -15,31 +15,44 @@ class MainActivity : AppCompatActivity() {
 
         val entradaPeso : EditText = findViewById(R.id.editTextText)
         val entradaAltura : EditText = findViewById(R.id.editTextText2)
-        val textoSalida : TextView = findViewById(R.id.textView3)
+        val textoSalida : TextView = findViewById(R.id.textView4)
         val botonCalculadora : Button = findViewById(R.id.button)
+        val imc : TextView = findViewById(R.id.textView5)
 
         botonCalculadora.setOnClickListener {
             var textoPeso = entradaPeso.text.toString().toDouble();
             var textoAltura = entradaAltura.text.toString().toDouble();
-
             var resultado : Double = (textoPeso/ Math.pow(textoAltura, 2.0));
 
-            when(true){
-                (resultado < 18.5) ->{
-                    textoSalida.text = "Usted sufre delgadez, debería comer más ";
-                }
-                (resultado in 18.5..24.9)->{
-                    textoSalida.text = "Su estado de forma es bueno.\n"
-                }
-                (resultado in 25.0..29.9) ->{
-                    textoSalida.text = "Usted sufre sobrepeso, debería cuidarse y llamar a un dietista\n"
-                }
-                (resultado >= 30) ->{
-                textoSalida.text = "Usted sufre obesidad, debería llamar a un médico urgentemente\n"
+            if(textoAltura > 0 && textoPeso > 0) {
+                when (true) {
+                    (resultado < 18.5) -> {
+                        textoSalida.text = "Usted sufre delgadez, debería comer más ";
+                    }
 
+                    (resultado in 18.5..24.9) -> {
+                        textoSalida.text = "Su estado de forma es bueno.\n"
+                    }
+
+                    (resultado in 25.0..29.9) -> {
+                        textoSalida.text =
+                            "Usted sufre sobrepeso, debería cuidarse y llamar a un dietista\n"
+                    }
+
+                    (resultado >= 30) -> {
+                        textoSalida.text =
+                            "Usted sufre obesidad, debería llamar a un médico urgentemente\n"
+
+                    }
+
+                    else -> {
+                        "Dato introducido erroneo"
+                    }
+                }
+            }else {
+                textoSalida.text = "Numero de peso o de altura introducido incorrectamente"
             }
-                else -> {"Dato introducido erroneo"}
-            }
+            imc.text =resultado.toString()
         }
 
 
