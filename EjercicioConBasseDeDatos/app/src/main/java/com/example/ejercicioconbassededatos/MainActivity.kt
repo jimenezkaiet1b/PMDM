fe646197bdad08151d6c42ca183eaa9aa9d8c8f8
Kaiet <kjimeneald@educacion.navarra.es>
2024-01-12 19:15:49 +0100

    SEGUIR

--- EjercicioConBasseDeDatos/app/src/main/java/com/example/ejercicioconbassededatos/MainActivity.kt
+++ EjercicioConBasseDeDatos/app/src/main/java/com/example/ejercicioconbassededatos/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.core.text.set
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityM
+    lateinit var personaDB : SQLite
+
     private lateinit var editTextDni : EditText
     private lateinit var editTextNombre:EditText
     private lateinit var editTextEdad:EditText
@@ -28,6 +31,13 @@ class MainActivity : AppCompatActivity() {
         editTextEdad = findViewById(R.id.edad)
         editTextProfesion = findViewById(R.id.profesion)
         editTextSalario = findViewById(R.id.salario)
+        Limpiar(R.id.dni)
+        Limpiar(R.id.nombre)
+        Limpiar(R.id.edad)
+        Limpiar(R.id.profesion)
+        Limpiar(R.id.salario)
+
+
 
     }
 
@@ -38,35 +48,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun insertar(view: View){
-        var datos = SQLite(this,"persona", null,1)
-        var baseDatos = datos.writableDatabase
-        var txtDNI = editTextDni?.text.toString()
-        var txtNombre = editTextNombre?.text.toString()
-        var txtEdad = editTextEdad?.text.toString()
-        var txtProfesion = editTextProfesion?.text.toString()
-        var txtSalario = editTextSalario?.text.toString()
+//        var datos = SQLite(this,"persona", null,1)
+//        var baseDatos = datos.writableDatabase
+//        var txtDNI = editTextDni?.text.toString()
+//        var txtNombre = editTextNombre?.text.toString()
+//        var txtEdad = editTextEdad?.text.toString()
+//        var txtProfesion = editTextProfesion?.text.toString()
+//        var txtSalario = editTextSalario?.text.toString()
 
-        Limpiar(R.id.dni)
-        Limpiar(R.id.nombre)
-        Limpiar(R.id.edad)
-        Limpiar(R.id.profesion)
-        Limpiar(R.id.salario)
 
-        if (txtDNI.isEmpty() && txtEdad.isEmpty() && txtNombre.isEmpty() && txtProfesion.isEmpty() && txtSalario.isEmpty()){
-            var registro = ContentValues()
-            registro.put("dni",txtDNI)
-            registro.put("nombre",txtNombre)
-            registro.put("edad",txtEdad)
-            registro.put("profesion",txtProfesion)
-            registro.put("salario",txtSalario)
 
-            baseDatos.insert("persona",null,registro)
-            Toast.makeText(this,"Se ha insertado existosamente",Toast.LENGTH_LONG).show()
-
-        }else{
-            Toast.makeText(this,"Los campos deben tener texto",Toast.LENGTH_LONG).show()
-
-        }
+//        if (txtDNI.isEmpty() && txtEdad.isEmpty() && txtNombre.isEmpty() && txtProfesion.isEmpty() && txtSalario.isEmpty()){
+//            var registro = ContentValues()
+//            registro.put("dni",txtDNI)
+//            registro.put("nombre",txtNombre)
+//            registro.put("edad",txtEdad)
+//            registro.put("profesion",txtProfesion)
+//            registro.put("salario",txtSalario)
+//
+//            baseDatos.insert("persona",null,registro)
+//            Toast.makeText(this,"Se ha insertado existosamente",Toast.LENGTH_LONG).show()
+//
+//        }else{
+//            Toast.makeText(this,"Los campos deben tener texto",Toast.LENGTH_LONG).show()
+//
+//        }
 
     }
 }
--- EjercicioConBasseDeDatos/app/src/main/java/com/example/ejercicioconbassededatos/SQLite.kt
+++ EjercicioConBasseDeDatos/app/src/main/java/com/example/ejercicioconbassededatos/SQLite.kt
@@ -1,8 +1,10 @@
 package com.example.ejercicioconbassededatos
 
+import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import org.w3c.dom.Text
 
 class SQLite(
     context: Context?,
@@ -11,12 +13,34 @@ class SQLite(
     version: Int
 ) : SQLiteOpenHelper(context, name, factory, version) {
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("create table persona (DNI texto primary key,nombre text,edad int,profesion text,salario int)")
+        val ordenCreacion = "CREATE TABLE persona (_dni INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, edad Int, profesion TEXT, Salario Int)"
+        db!!.execSQL(ordenCreacion)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
-        TODO("Not yet implemented")
+    override fun onUpgrade(db: SQLiteDatabase?,
+                           oldVersion: Int, newVersion: Int) {
+        val ordenBorrado = "DROP TABLE IF EXISTS persona"
+        db!!.execSQL(ordenBorrado)
+        onCreate(db)
+    }
+
+    fun anadirDato(dni : String,nombre: String, edad: Int, profesion:String, salario : Int){
+        val datos = ContentValues()
+        datos.put("dni",dni)
+        datos.put("nombre",nombre)
+        datos.put("edad",edad)
+        datos.put("profesion",profesion)
+        datos.put("salario",salario)
+
+        val db = this.writableDatabase
+        db.insert("persona",null, datos)
+        db.close()
+
+
+
     }
 
 
 }
+
+
